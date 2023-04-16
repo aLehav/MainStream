0d1d0c6c68a0ec7a80f2350165ec99ac9bf457ef
TobyFenn <tfenner@usc.edu>
2023-04-16 00:58:39 -0700

    store genre page selections & add highlights

--- mainstream/src/pages/GenrePage/GenrePage.jsx
+++ mainstream/src/pages/GenrePage/GenrePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './GenrePage.css';
 import { useNavigate } from 'react-router-dom';
 
@@ -12,6 +12,18 @@ function GenrePage() {
     'Reggaeton', 'RnB', 'Folk', 'Oldies', '80s', 'Classic Rock'
   ];
 
+ // State to keep track of the clicked genres
+ const [clickedGenres, setClickedGenres] = useState([]);
+
+ const handleGenreClick = (genre) => {
+   // Add or remove the clicked genre from the state
+   if (clickedGenres.includes(genre)) {
+     setClickedGenres(clickedGenres.filter(g => g !== genre));
+   } else {
+     setClickedGenres([...clickedGenres, genre]);
+   }
+ };
+
   const handleNextClick = () => {
     // Navigate to the next page
     navigate('/community');
@@ -22,11 +34,15 @@ function GenrePage() {
       <h1>Your favorite genres...</h1>
       <h2>Choose these to get songs curated towards who you are</h2>
       <div className="genre-grid">
-        {genres.map((genre, index) => (
-          <div className="genre-square" key={index}>
-            {genre}
-          </div>
-        ))}
+       {genres.map((genre, index) => (
+         <div 
+           className={`genre-square ${clickedGenres.includes(genre) ? 'clicked' : ''}`} 
+           key={index} 
+           onClick={() => handleGenreClick(genre)}
+         >
+           {genre}
+         </div>
+       ))}
       </div>
       <button onClick={handleNextClick}>Next</button>
     </div>
